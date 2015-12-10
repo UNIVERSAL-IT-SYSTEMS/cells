337ad888ded4fa8c16e0e7919581c86aa0d5ff88
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-12-10 07:32:36 -0800

    matrix intializer, reader

--- test/test.js
+++ test/test.js
@@ -2,17 +2,31 @@
 
 var matrix = require('../lib/');
 
+var stdlib = {
+    Math: Math,
+    Uint32Array: Uint32Array
+};
+
 var heap = new ArrayBuffer(0x10000);
-var stdlib = { Math: Math };
-var m = matrix(stdlib, null, heap);
+var m = matrix.box(stdlib, null, heap);
+var hu32 = new Uint32Array(heap);
 
 describe('test', function () {
     it('uniqueCols', function (done) {
-        m.uniqueCols(0,0);
+        matrix.write(heap, 0,
+            '...1111...1....\
+             ...1111...1.2..\
+             ...1111...1.2..\
+             ..3333....1....\
+             ..3333....111..\
+             ...............'
+        );
+        m.uniqueCols(0);
+        console.log(matrix.read(heap, 0));
         done();
     });
     it('uniqueRows', function (done) {
-        m.uniqueRows(0,0);
+        m.uniqueRows(0);
         done();
     });
 });
